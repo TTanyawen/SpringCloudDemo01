961cd6d7207bccf09117ca3cb301202194ec382f
Tan Yawen <2749934612@qq.com>
2025-06-26 16:50:46 +0800

    定义规则

--- SpringCloudDemo01/cloud-consumer-order9001/src/main/java/com/example/OrderMain9001.java
+++ SpringCloudDemo01/cloud-consumer-order9001/src/main/java/com/example/OrderMain9001.java
@@ -1,10 +1,13 @@
 package com.example;
 
 
+import com.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderMain9001 {
