7a11aee1b5e96d4af1a76b7ec938087dfdeca8dd
Tan Yawen <2749934612@qq.com>
2025-06-27 09:34:58 +0800

    服务降级(超时或者有问题的话就走配置的兜底方法，而不是一直等待)

--- SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/controller/OrderHystrixController.java
+++ SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/controller/OrderHystrixController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 
 import com.example.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +21,28 @@ public class OrderHystrixController {
         log.info("*******result:"+result);
         return result;
     }
+//    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+//    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+//        String result = paymentHystrixService.paymentInfo_Timeout(id);
+//        log.info("*******result:"+result);
+//        return result;
+//    }
+
+
+
+    //超时降级演示
+    @HystrixCommand(fallbackMethod = "payment_TimeoutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")            //超过1.5秒就降级自己
+    })
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        //int age= 1/0;
         String result = paymentHystrixService.paymentInfo_Timeout(id);
         log.info("*******result:"+result);
         return result;
+    }
+    //兜底方法，上面方法出问题,我来处理，返回一个出错信息
+    public String payment_TimeoutHandler(Integer id) {
+        return "我是消费者9001,对方支付系统繁忙请10秒后再试。或自己运行出错，请检查自己。";
     }
 }
--- SpringCloudDemo01/cloud-provider-hystrix-payment8001/src/main/java/com/example/service/impl/PaymentServiceImpl.java
+++ SpringCloudDemo01/cloud-provider-hystrix-payment8001/src/main/java/com/example/service/impl/PaymentServiceImpl.java
@@ -1,23 +1,57 @@
 package com.example.service.impl;
 
 import com.example.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.TimeUnit;
 @Service
 public class PaymentServiceImpl implements PaymentService {
     //成功
     public String paymentInfo_OK(Integer id) {
-        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "访问成功，哈哈哈~~~";
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
     }
 
-    //失败
+    //超时降级演示
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")           //5秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_Timeout(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 15;//超过5s，走配置的降级方法，有异常也是走降级方法
+//        int i=1/0;  //除0错误
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "访问失败，呜呜呜~~" + " 耗时(秒)" + timeNumber;
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+    }
+
+    //兜底方法，上面方法出问题,我来处理，返回一个出错信息
+    public String paymentInfo_TimeoutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + " paymentInfo_TimeoutHandler,系统繁忙,请稍后再试\t o(╥﹏╥)o ";
     }
 }
+
+//
+//import com.example.service.PaymentService;
+//import org.springframework.stereotype.Service;
+//import java.util.concurrent.TimeUnit;
+//@Service
+//public class PaymentServiceImpl implements PaymentService {
+//    //成功
+//    public String paymentInfo_OK(Integer id) {
+//        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "访问成功，哈哈哈~~~";
+//    }
+//
+//    //失败
+//    public String paymentInfo_Timeout(Integer id) {
+//        int timeNumber = 3;
+//        try {
+//            TimeUnit.SECONDS.sleep(timeNumber);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "访问失败，呜呜呜~~" + " 耗时(秒)" + timeNumber;
+//    }
+//}
