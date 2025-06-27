ad85b7fbb55f6cf7071da5ceac0344630352477f
Tan Yawen <2749934612@qq.com>
2025-06-27 09:47:10 +0800

    服务降级：解耦，做一个处理降级的Service，不要在controller里头写了

--- SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/controller/OrderHystrixController.java
+++ SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/controller/OrderHystrixController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 @RestController
 @Slf4j
-@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod") //没有配置特定的兜底方法的话就用通用配置的这一个
+//@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod") //没有配置特定的兜底方法的话就用通用配置的这一个
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -36,7 +36,7 @@ public class OrderHystrixController {
 //    @HystrixCommand(fallbackMethod = "payment_TimeoutHandler",commandProperties = {
 //            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")            //超过1.5秒就降级自己
 //    })
-    @HystrixCommand
+//    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         //int age= 1/0;
@@ -44,14 +44,19 @@ public class OrderHystrixController {
         log.info("*******result:"+result);
         return result;
     }
+
+
+    //解耦了，把降级处理在Service接口配置，专门做一个ServiceFallBack的Service来处理，不用放在控制器来写
+
+
     //兜底方法，上面方法出问题,我来处理，返回一个出错信息
-    public String payment_TimeoutHandler(Integer id) {
-        return "我是消费者9001,对方支付系统繁忙请10秒后再试。或自己运行出错，请检查自己。";
-    }
+//    public String payment_TimeoutHandler(Integer id) {
+//        return "我是消费者9001,对方支付系统繁忙请10秒后再试。或自己运行出错，请检查自己。";
+//    }
 
 
     //下面是全局fallback方法
-    public String payment_Global_FallbackMethod(){
-        return "Global异常处理信息，请稍后再试,(┬＿┬)";
-    }
+//    public String payment_Global_FallbackMethod(){
+//        return "Global异常处理信息，请稍后再试,(┬＿┬)";
+//    }
 }
--- SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/service/PaymentHystrixService.java
+++ SpringCloudDemo01/cloud-consumer-feign-hystrix-order9001/src/main/java/com/example/service/PaymentHystrixService.java
@@ -2,10 +2,13 @@ package com.example.service;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("CLOUD-HYSTRIX-PAYMENT-SERVICE")
+//@FeignClient("CLOUD-HYSTRIX-PAYMENT-SERVICE")
+@Component
+@FeignClient(value = "CLOUD-HYSTRIX-PAYMENT-SERVICE",fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService {
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id);
